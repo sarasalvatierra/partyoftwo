2c29b9541551543e96c77b20c2c2df23053b3eec
sarasalvatierra <saraisalvatierra@gmail.com>
2023-05-03 18:52:40 -0400

    created Home page, added navigation bar

--- src/App.js
+++ src/App.js
@@ -1,19 +1,11 @@
 import React from 'react';
 import { Routes, Route } from 'react-router-dom';
-import Login from './screens/Login';
-import Register from './screens/Register';
-import Users from './screens/Users';
-import Campaigns from './screens/Campaigns';
-import Dashboard from './screens/Dashboard';
+import Home from './screens/Home';
 
 function App() {
   return (
     <Routes>
-      <Route path="/" element={<Login />} />
-      <Route path="register" element={<Register />} />
-      <Route path="users" element={<Users />} />
-      <Route path="campaigns" element={<Campaigns />} />
-      <Route path="dashboard" element={<Dashboard />} />
+      <Route path="/" element={<Home />} />
     </Routes>
   );
 }
