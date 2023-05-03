68e107205f0846e9ec390a8b9ae98dabf109f25d
sarasalvatierra <saraisalvatierra@gmail.com>
2023-05-03 18:01:59 -0400

    installed antd package

--- src/App.js
+++ src/App.js
@@ -1,11 +1,20 @@
-import './App.css';
+import React from 'react';
+import { Routes, Route } from 'react-router-dom';
+import Login from './screens/Login';
+import Register from './screens/Register';
+import Users from './screens/Users';
+import Campaigns from './screens/Campaigns';
+import Dashboard from './screens/Dashboard';
 
-const App = () => {
+function App() {
   return (
-    <div className="App">
-      <h1>Party of Two</h1>
-    </div>
+    <Routes>
+      <Route path="/" element={<Login />} />
+      <Route path="register" element={<Register />} />
+      <Route path="users" element={<Users />} />
+      <Route path="campaigns" element={<Campaigns />} />
+      <Route path="dashboard" element={<Dashboard />} />
+    </Routes>
   );
 }
-
 export default App;
